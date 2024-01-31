a8d54701bc7a39e7b4dd258732e0b58c9ee93ebb
yong park <yong.park123456@gmail.com>
2024-01-30 23:19:06 -0600

    afin brutefoce done

--- afine_brute.py
+++ afine_brute.py
@@ -1,25 +1,27 @@
-from prettytable import PrettyTable
+import math
 
 alfabeto = 'abcdefghijklmnñopqrstuvwxyz'
 list_abcedario = list(alfabeto)
 
 def afin_desencrtypt(word, a_value, b_value):
-    encripted = []
+    decrypted = []
     word = list(word.lower())
     for x in word:
         if x in list_abcedario:
             indice = list_abcedario.index(x)
-            # print("avalue: ", a_value)
-            a_inverse = (a_value**-1) % len(list_abcedario)
-            # a_inverse = pow(a_value, -1)%len(list_abcedario)
-            # print("ainverse: ", a_inverse)
-            # print()
-            position = int((a_inverse * (indice - b_value)) % len(list_abcedario) )
-            encripted.append(list_abcedario[position])
+            if math.gcd(a_value, len(list_abcedario)) == 1:
+                a_inverse = pow(a_value, -1, len(list_abcedario))
+                position = int((a_inverse * (indice - b_value)) % len(list_abcedario))
+            else:
+                # Si 'a' no es coprimo, utiliza una lógica diferente
+                position = int((a_value * (indice - b_value)) % len(list_abcedario))
+            decrypted.append(list_abcedario[position])
         else:
-            encripted.append(x)
-    encripted = "".join(encripted)
-    return encripted
+            decrypted.append(x)
+    decrypted = "".join(decrypted)
+    return decrypted
+
+
 
 print("Ingrese la palabra: ")
 word = input()
@@ -32,3 +34,4 @@ for a in range(1,27):
 
 
 
+QHNHVPEKHVHVRRMIRYERARMTECJZRVRYEQCBHIRPPCLBRJRQHRPBXERPIBCJIRPJHZCBJHQQHHEPBPHVRKEHIRNHQHRQHACBJTERIPHYBEJQBIRVRIPRMZCRJIHMVCRLNCQVBMCMVRAHZEJHMXEIJCUATERMRPACPHJXHPHZBJICJEHPZBJRQXQHJJHZCBJHQVRAHZEJHZCBJZBJIPHRQZBPBJHACPEMBPCKCJHQNRJIRRQHACBJCDHHHIRPPCLHPRJKEHIRNHQHRQNCRPZBQRMHQHMACRCJICEJHGBPHMXRPBQERKBMRCJÑBPNBTERRQAERQBMRGHDCHPRIPHMHVBOXBPRMBQQRKBHQXHCMHRMBVRQHMZEHIPBGBPHMVRRMIRYERARMRQNCJCMIRPCBVRMHQEVCJÑBPNBTERRQHACBJIPHYBIPRMZCRJIHMVCRLNCQVBMCMVRQDCBQBKCZBPEMBVRQHMZEHQRMMRMRJIHNCQMBJMRKEJVHMVBMCMOVBMZCRJIBMZCJZERJIHNCQXPCNRPHMVBMCMHVCÑRPRJZCHVRBIPHMAHZEJHMRQDCBQBKCZBVRPEMCHICRJRVBMZBNXBJRJIRMEJBTERMREICQCLHXHPHQHXPCNRPHVBMCMOBIPBXHPHRQPRÑERPLBORMIRZHPKHNRJIBVRMRMRJIHNCQMCKJCÑCZHPCHRQXPCNRPBTERPRZCDRRQXHCMVRMRKEJVHMVBMCMZBJRMIHMVBMCMMRZBJICJEHPHZBJRQXQHJJHZCBJHQVRAHZEJHZCBJOMRCJCZCHPHZBJQHHVNCJCMIPHZCBJVRQHMMRKEJVHMVBMCMZEHJVBQHMXPCNRPHMXRPMBJHMCJNEJCLHVHMZBJMXEIJCUAZENXQHJMEMJBARJIHVCHMVRGHDRPPRZCDCVBQHXPCNRPHVBMCMZCIHQHCJÑBPNHZCBJVRQNCJCMIRPCBVRMHQEV
